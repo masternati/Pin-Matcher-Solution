c73ce21b515d6c0bfc2b2e481c85e3dbee6d4c74
masternati <sohrabhossain207@gmail.com>
2023-10-01 22:02:03 +0600

    pin matches  is successful, if the pin is matched with generated pin show a success message otherwise show a failure message

--- JS/script.js
+++ JS/script.js
@@ -8,12 +8,12 @@ function getPin() {
     else{
         return getPin();
     }
-}
+};
 
 function generatePin() {
     const random = Math.round(Math.random()*10000);
     return random;
-}
+};
 
 document.getElementById('generate-pin').addEventListener('click', function () {
     const pin = getPin();
@@ -21,7 +21,7 @@ document.getElementById('generate-pin').addEventListener('click', function () {
     // display pin 
     const displayPin = document.getElementById('display-pin');
     displayPin.value = pin;
-})
+});
 
 // input pin
 document.getElementById('calculator').addEventListener('click', function typePinNumber(event){
@@ -40,8 +40,31 @@ document.getElementById('calculator').addEventListener('click', function typePin
         }
     }
     else{
-        
         const newPinNumber = previousTypeNumber + num;
         typePinNumber.value = newPinNumber;
     } 
 });
+
+const successMessage = document.getElementById('pin-success');
+const failureMessage = document.getElementById('pin-failure');
+failureMessage.style.display = 'none';
+successMessage.style.display = 'none';
+
+document.getElementById('verify-pin').addEventListener('click', function messageDisplay () {
+    const displayedPin = document.getElementById('display-pin');
+    const currenPin = displayedPin.value;
+
+    const typedPin = document.getElementById('typed-pin');
+    const typedPinDigits = typedPin.value;
+
+
+    if (currenPin == typedPinDigits) {
+        successMessage.style.display = 'block';
+        failureMessage.style.display = 'none';
+    } 
+    else {
+        failureMessage.style.display = 'block';
+        successMessage.style.display = 'none';
+    }
+   
+})
